c1b30cc91d660a18121c6f10b24f99046a757686
esther-maker <52613916+esther-maker@users.noreply.github.com>
2021-04-14 21:36:54 -0400

    Add files via upload

---
+++ conexion_db.php
@@ -0,0 +1,155 @@
+<?php
+
+  
+$Conexion = mysqli_connect("localhost","root","","prueba");
+
+
+
+if(isset($_POST['boton'])){
+
+
+if(strlen($_POST['Usuario']) >= 1 && strlen($_POST['Contraseña']) >= 1){
+
+  $Usuario = $_POST['Usuario'];
+  $Contraseña = $_POST['Contraseña'];
+
+  $Consulta = mysqli_query($Conexion, "SELECT * FROM usuarios WHERE nombre = '$Usuario' and contraseña= '$Contraseña'");
+
+  $Verificacion = mysqli_num_rows($Consulta);
+
+  if($Verificacion == 1){
+    session_start();
+    $_SESSION['user'] = $Usuario;
+  
+    echo '
+       <script>
+          
+         alert("Ha ingresado");
+         window.location.replace("index.php");
+       </script>
+       
+    ';
+  }
+
+  else{
+    echo '
+       <script>
+         alert("Contraseña/ usuario incorrectos");
+       </script>
+    ';
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
